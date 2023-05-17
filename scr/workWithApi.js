39a571349d559bff9f059af1889c50db88a03d1e
Valeryaleshka <53650916+Valeryaleshka@users.noreply.github.com>
2023-05-17 19:12:39 -0300

    Update workWithApi.js

--- scr/workWithApi.js
+++ scr/workWithApi.js
@@ -3,7 +3,7 @@ import { createOptionsForSelecters } from "./functions.js";
 let countries = [];
 
 export function fillCities(countryName, citypicker) {
-  fetch("https://restcountries.eu/rest/v2/all")
+  fetch("https://restcountries.com/v3.1/all")
     .then((response) => {
       if (response.ok) {
         const data = response.json();
